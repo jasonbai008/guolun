bae410760d75c215a028cb9af8050a0db53441ed
jasonbai <15810683857@163.com>
2025-03-15 21:14:10 +0800

    扩展播放列表

--- utils/rename.js
+++ utils/rename.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // 设置起始数字（作为字符串）
-const startNumber = '004'; // 可以修改这个值
+const startNumber = '252'; // 可以修改这个值
 const mp3Directory = path.join(__dirname, 'mp3');
 
 // 读取目录下的文件
@@ -13,14 +13,14 @@ fs.readdir(mp3Directory, (err, files) => {
     }
 
     // 过滤出符合条件的 .mp3 文件
-    const mp3Files = files.filter(file => {
-        return file.endsWith('.mp3') && /^\d{3}\..+\.mp3$/.test(file);
-    });
+    // const mp3Files = files.filter(file => {
+    //     return file.endsWith('.mp3') && /^\d{3}\..+\.mp3$/.test(file);
+    // });
 
     // 按照文件名的顺序进行重命名
-    mp3Files.sort(); // 这里按照文件名的字典序排序
+    // mp3Files.sort(); // 这里按照文件名的字典序排序
 
-    mp3Files.forEach((file, index) => {
+    files.forEach((file, index) => {
         const newNumber = String(parseInt(startNumber) + index).padStart(3, '0'); // 确保是3位数
         const newFileName = `${newNumber}.${file.split('.')[1]}.mp3`; // 保留中文字符部分
         const oldFilePath = path.join(mp3Directory, file);
